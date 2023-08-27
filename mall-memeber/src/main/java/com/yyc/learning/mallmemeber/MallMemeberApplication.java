dc9589a0a786c26eb3c9f86871d9c19bfe88d376
yyc <dyang410@gmail.com>
2023-08-27 00:12:59 -0400

    project initial build

---
+++ mall-memeber/src/main/java/com/yyc/learning/mallmemeber/MallMemeberApplication.java
@@ -0,0 +1,13 @@
+package com.yyc.learning.mallmemeber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallMemeberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallMemeberApplication.class, args);
+    }
+
+}
---
+++ mall-memeber/src/test/java/com/yyc/learning/mallmemeber/MallMemeberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyc.learning.mallmemeber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMemeberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
